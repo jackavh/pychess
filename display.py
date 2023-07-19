6b15fde9e43bcd7f8023f5ae57a85468b0e3346a
Jack Hansen <47556286+jackavh@users.noreply.github.com>
2023-07-18 19:39:21 -0700

    Adds piece drawing functionality

--- display.py
+++ display.py
@@ -1,11 +1,52 @@
 import arcade
 from helper import *
+import os
 
 
+# Size globals
+PADDING = 64
+SQUARE_SIZE = 128
+SCREEN_WIDTH = 8 * SQUARE_SIZE + PADDING
+SCREEN_HEIGHT = 8 * SQUARE_SIZE + PADDING
+SCREEN_TITLE = "pyChess"
+
+# Color globals
 HIGHLIGHT_COLOR = (0xFF, 0xa1, 0xa1)
 WHITE_COLOR = (0xEF, 0xD9, 0xB5)
 BLACK_COLOR = (0xb4, 0x88, 0x63)
 BACKGROUND  = (0x29, 0x27, 0x21)
+
+# Spritesheet globals
+PIECE_SPRITE_SHEET = os.path.join(os.getcwd(), 'assets', 'Chess_Pieces_Sprite.png')
+PIECE_SPRITES = arcade.load_spritesheet(PIECE_SPRITE_SHEET,
+                                        3840 / 6, 1280 / 2,
+                                        6, 12, 0)
+
+
+class DisplayPiece(arcade.Sprite):
+
+    def __init__(self, piece: int):
+        super().__init__()
+        self.scale = SQUARE_SIZE * (1/640)
+        self.texture = PIECE_SPRITES[get_sprite_from_piece(piece)]
+        
+
+
+    def set_board_position(self, idx):
+        """
+        Sets the coordinates of the sprite based on board index
+        """
+        x, y = flat_to_xy(idx)
+        self.center_x = x * SQUARE_SIZE + SQUARE_SIZE / 2 + PADDING / 2
+        self.center_y = y * SQUARE_SIZE + SQUARE_SIZE / 2 + PADDING / 2
+
+    
+    def set_xy_board_position(self, x, y):
+        """
+        Sets the coordinates of the sprite based on board position
+        """
+        self.center_x = x * SQUARE_SIZE + SQUARE_SIZE / 2 + PADDING / 2
+        self.center_y = y * SQUARE_SIZE + SQUARE_SIZE / 2 + PADDING / 2
 
 
 class Display(arcade.Window):
@@ -16,6 +57,7 @@ class Display(arcade.Window):
         self.padding = 0 if padding is None else padding
         self.highlights = None
         self.hover_square = None
+        self.piece_sprites = None
         
         super().__init__(width, height, title)
         arcade.set_background_color(BACKGROUND)
@@ -45,13 +87,49 @@ class Display(arcade.Window):
                                                   col)
         
         # DEBUG: write the hover square to the screen
-        debug_str = f'Hover square: {self.hover_square}\nHighlighted: {self.highlights}'
-        arcade.draw_text(debug_str, 16, self.display_size[1] - 32, arcade.color.BLACK, 16,
-                         multiline=True, width=self.display_size[0] - 32)
+        # debug_str = f'Hover square: {self.hover_square}\nHighlighted: {self.highlights}'
+        # arcade.draw_text(debug_str, 16, self.display_size[1] - 32, arcade.color.BLACK, 16,
+        #                  multiline=True, width=self.display_size[0] - 32)
+
+        # Draw the pieces
+        self.piece_sprites.draw()
+
+        
+
+    def add_piece(self, piece: int, idx: int):
+        p = DisplayPiece(piece)
+        p.set_board_position(idx)
+        self.piece_sprites.append(p)
 
     
     def setup(self):
         self.highlights = set()
+        self.piece_sprites = arcade.SpriteList()
+        
+        
+        # DEBUG: For fun, draw all the pieces on the board manually
+        self.add_piece(0b01101, 0) # white rook
+        self.add_piece(0b01100, 1) # white knight
+        self.add_piece(0b01011, 2) # white bishop
+        self.add_piece(0b01010, 3) # white queen
+        self.add_piece(0b01001, 4) # white king
+        self.add_piece(0b01011, 5) # white bishop
+        self.add_piece(0b01100, 6) # white knight
+        self.add_piece(0b01101, 7) # white rook
+        for i in range(8, 16):
+            self.add_piece(0b01110, i) # white pawns
+        self.add_piece(0b10101, 56) # black rook
+        self.add_piece(0b10100, 57) # black knight
+        self.add_piece(0b10011, 58) # black bishop
+        self.add_piece(0b10010, 59) # black queen
+        self.add_piece(0b10001, 60) # black king
+        self.add_piece(0b10011, 61) # black bishop
+        self.add_piece(0b10100, 62) # black knight
+        self.add_piece(0b10101, 63) # black rook
+        for i in range(48, 56):
+            self.add_piece(0b10110, i) # black pawns
+
+
 
 
     def update(self, delta_time):
@@ -105,3 +183,15 @@ class Display(arcade.Window):
 
     def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
         pass
+
+
+# Run the game
+def main():
+    game = Display(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE,
+                   square_size=SQUARE_SIZE, padding=PADDING)
+    game.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
--- helper.py
+++ helper.py
@@ -1,9 +1,42 @@
+# Chess encoding methods
+# No piece is 0, indexed from sprite sheet
+pieces = { 'k': 1, 'q': 2, 'b': 3,
+           'n': 4, 'r': 5, 'p': 6, }
+pieces_inverse = {v: k for k, v in pieces.items()}
+
+def get_sprite_from_piece(p: int) -> int:
+    """Returns the sprite index from a piece encoding"""
+    # White pieces are 0-5, black pieces are 6-11
+    index = get_char_from_piece(p).lower()
+    color_offset = 6 * (not color_of(p)) # color_of returns 1 for white and 0 for black
+    return pieces[index] + color_offset - 1
+
+def get_piece_from_char(c: str) -> int:
+    """Returns the piece encoding from a character"""
+    color = 8 if c.isupper() else 16;
+    return color ^ pieces[c.lower()]
+
+
+def get_char_from_piece(p: int) -> str:
+    """Returns the character of a piece from a piece encoding"""
+    is_white = p & 8
+    c = pieces_inverse[p & 7]
+    return c.upper() if is_white else c
+
+
+def color_of(p: int) -> int:
+    """Returns the color of a piece as 1 as white or 0 as black from a piece encoding"""
+    return 1 if p & 8 else 0
+
+
 # Coordinate methods
-def xy_to_flat(x, y):
+def xy_to_flat(x: int, y: int) -> int:
+    """Converts x, y coordinates to a flat index 0-63"""
     return y * 8 + x
 
 
-def flat_to_xy(i):
+def flat_to_xy(i: int) -> tuple:
+    """Converts a flat index 0-63 to x, y coordinates"""
     return i % 8, i // 8
 
 
